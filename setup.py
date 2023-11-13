e92320ba3938d71b5b3799bf411c96f4427054ac
Björn Wächter <bjoern@waechterei.de>
2023-11-13 11:40:33 +0100

    Use correct bot email in action and set pyarrow version in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
    packages=find_packages(include=['osm_address', 'osm_address.*']),
    install_requires=[
       'apache-sedona>=1.5.0',
-      'pyarrow==14.0.0',
+      'pyarrow==14.0.1',
       'pyspark==3.3.1',
       'shapely==2.0.2'
    ]
